863992381cf77111b38258e944984ba04e6963dd
DoraShao <shaoyc@live.com>
2015-10-04 15:27:07 -0400

    Initial Commit

---
+++ QuickNote/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QuickNote
+//
+//  Created by Yicheng Shao on 10/4/15.
+//  Copyright (c) 2015 Dora Shao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
